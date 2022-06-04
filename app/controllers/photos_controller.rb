7558b4bda7afa888b57068a99ea107b8dbecbc79
Aicha Diagne <awdstrategy@gmail.com>
2022-06-04 16:50:03 +0200

    added a username to User and created the photo gallery view

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,5 @@
 class PhotosController < ApplicationController
+  def user_gallery
+    @user = User.find_by(username: params[:username])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :photos
   has_one_attached :photo
+
+  validates :username, uniqueness: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
