724b0da31a3f51ca3095c84cf830e8b861570a67
Aicha Diagne <awdstrategy@gmail.com>
2022-06-16 19:08:36 +0200

    slightly modified self.create_private_chatroom

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -4,6 +4,6 @@ class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   #after_create_commit { broadcast_append_to "chatrooms"}
   def self.create_private_chatroom(first_user, second_user )
-   Chatroom.create(first_user: first_user, second_user: second_user )
+   Chatroom.create(first_user_id: first_user.id, second_user_id: second_user.id)
   end
 end
