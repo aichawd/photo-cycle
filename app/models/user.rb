c00ddd26405d2fbf65210a35ca82aff4fb71a60d
Kenza Tghrn <kenza_tighrine@hotmail.fr>
2022-06-18 09:37:37 +0200

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :pictures
-  has_many :chatrooms, dependent: :destroy
-  has_many :messages, through: :chatrooms
+  has_many :pictures, dependent: :destroy
   has_one_attached :photo
+  has_many :chatrooms_as_first_user, :class_name => 'Chatroom', :foreign_key => 'first_user_id', dependent: :destroy
+  has_many :chatrooms_as_second_user, :class_name => 'Chatroom', :foreign_key => 'second_user_id', dependent: :destroy
 
   validates :username, uniqueness: true
 
