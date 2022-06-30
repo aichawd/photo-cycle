701dee03f359ff5fad4eda61f40be9d54f1e8dec
Aicha Diagne <awdstrategy@gmail.com>
2022-06-30 23:08:56 +0200

    aligning collab banner with user banner and adding stories to collabs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_202717) do
+ActiveRecord::Schema.define(version: 2022_06_30_205541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_06_29_202717) do
     t.integer "artist1_id"
     t.integer "artist2_id"
     t.text "description"
+    t.string "story"
   end
 
   create_table "messages", force: :cascade do |t|
